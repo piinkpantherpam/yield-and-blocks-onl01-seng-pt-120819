950d986e9451cbfe9d795c543724d44689c5b299
Pam Cairo <pcairo_90@outlook.com>
2019-12-21 03:22:56 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,2 +1,24 @@
+["Tim", "Tom", "Jim"].each do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
 
 
+
+def hello_t(array)
+    i = 0 
+  
+    while i < array.length
+      i = i + 1 
+  end
+end
+
+def hello_t(array)
+  i = log10
+  
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+end
